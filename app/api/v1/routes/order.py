af5401193c2728e5c0322b6959b60b570a4e78d6
Emmanuel <emmanuelchiedueke01@gmail.com>
2026-04-04 14:32:04 +0100

    feat(order_routes): implement store isolation for order routes

--- app/api/v1/routes/order.py
+++ app/api/v1/routes/order.py
@@ -3,7 +3,7 @@ from app.api.v1.models import (
     PaginatedMetadata,
     StandardResponse,
 )
-from fastapi import APIRouter, Depends, Query
+from fastapi import APIRouter, Depends, Query, BackgroundTasks
 from app.services import order_service
 from sqlalchemy.ext.asyncio import AsyncSession
 from app.auth.verify_jwt import verify_token
@@ -15,21 +15,29 @@ router = APIRouter(prefix="/shopping", tags=["Order"])
 
 @router.post("/orders")
 async def create_orders(
+    store_id: int,
     db: AsyncSession = Depends(get_db),
     payload: dict = Depends(verify_token),
 ):
-    return await order_service.create_orders(db=db, payload=payload)
+    return await order_service.create_orders(store_id=store_id, db=db, payload=payload)
 
 
 @router.post("/order_items")
 async def create_orderitems(
+    store_id: int,
     cart_id: int,
     order_id: int,
+    background_tasks: BackgroundTasks,
     db: AsyncSession = Depends(get_db),
     payload: dict = Depends(verify_token),
 ):
     return await order_service.create_order_items(
-        cart_id=cart_id, order_id=order_id, db=db, payload=payload
+        store_id=store_id,
+        cart_id=cart_id,
+        order_id=order_id,
+        background_tasks=background_tasks,
+        db=db,
+        payload=payload,
     )
 
 
@@ -40,13 +48,14 @@ async def create_orderitems(
     response_model_exclude_defaults=True,
 )
 async def view_orders(
+    store_id: int,
     page: int = Query(1, ge=1),
     limit: int = Query(10, le=100),
     db: AsyncSession = Depends(get_db),
     payload: dict = Depends(verify_token),
 ):
     return await order_service.view_orders(
-        db=db, payload=payload, page=page, limit=limit
+        store_id=store_id, db=db, payload=payload, page=page, limit=limit
     )
 
 
@@ -57,6 +66,7 @@ async def view_orders(
     response_model_exclude_defaults=True,
 )
 async def view_order(
+    store_id: int,
     order_id: int,
     page: int = Query(1, ge=1),
     limit: int = Query(10, le=100),
@@ -64,23 +74,34 @@ async def view_order(
     payload: dict = Depends(verify_token),
 ):
     return await order_service.view_order(
-        order_id=order_id, db=db, payload=payload, page=page, limit=limit
+        store_id=store_id,
+        order_id=order_id,
+        db=db,
+        payload=payload,
+        page=page,
+        limit=limit,
     )
 
 
 @router.put("/cancel_order")
 async def cancel(
     order_id: int,
+    store_id: int,
     db: AsyncSession = Depends(get_db),
     payload: dict = Depends(verify_token),
 ):
-    return await order_service.cancel_order(order_id=order_id, db=db, payload=payload)
+    return await order_service.cancel_order(
+        store_id=store_id, order_id=order_id, db=db, payload=payload
+    )
 
 
 @router.delete("/delete_order")
 async def delete_order(
     order_id: int,
+    store_id: int,
     db: AsyncSession = Depends(get_db),
     payload: dict = Depends(verify_token),
 ):
-    return await order_service.delete_order(order_id=order_id, db=db, payload=payload)
+    return await order_service.delete_order(
+        store_id=store_id, order_id=order_id, db=db, payload=payload
+    )
