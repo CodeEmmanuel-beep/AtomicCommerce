b48247cbac5f90f998316ea34cc2621a374d6df1
Emmanuel <emmanuelchiedueke01@gmail.com>
2026-04-06 19:19:51 +0100

    feat(order): add delivery address routes

--- app/api/v1/routes/order.py
+++ app/api/v1/routes/order.py
@@ -1,5 +1,7 @@
-from app.api.v1.models import (
+from app.api.v1.schemas import (
     OrderResponse,
+    AddressDetails,
+    AddressResponse,
     PaginatedMetadata,
     StandardResponse,
 )
@@ -36,6 +38,81 @@ async def create_orderitems(
         cart_id=cart_id,
         order_id=order_id,
         background_tasks=background_tasks,
+        db=db,
+        payload=payload,
+    )
+
+
+@router.post("/delivery_address")
+async def add_delivery_address(
+    store_id: int,
+    order_id: int,
+    delivery_address: AddressDetails,
+    background_task: BackgroundTasks,
+    db: AsyncSession = Depends(get_db),
+    payload: dict = Depends(verify_token),
+):
+    return await order_service.delivery_address(
+        store_id=store_id,
+        order_id=order_id,
+        delivery_address=delivery_address,
+        background_task=background_task,
+        db=db,
+        payload=payload,
+    )
+
+
+@router.get(
+    "/view_delivery_address",
+    response_model=StandardResponse[AddressResponse],
+    response_model_exclude_none=True,
+    response_model_exclude_defaults=True,
+)
+async def get_delivery_address(
+    store_id: int,
+    page: int = Query(1, ge=1),
+    limit: int = Query(10, le=100),
+    db: AsyncSession = Depends(get_db),
+    payload: dict = Depends(verify_token),
+):
+    return await order_service.view_delivery_address(
+        store_id=store_id,
+        page=page,
+        limit=limit,
+        db=db,
+        payload=payload,
+    )
+
+
+@router.put("/pick_delivery_address")
+async def update_delivery_address(
+    store_id: int,
+    order_id: int,
+    address_id: int,
+    db: AsyncSession = Depends(get_db),
+    payload: dict = Depends(verify_token),
+):
+    return await order_service.choose_order_address(
+        store_id=store_id,
+        order_id=order_id,
+        address_id=address_id,
+        db=db,
+        payload=payload,
+    )
+
+
+@router.delete("/delete_delivery_address")
+async def delete_address(
+    store_id: int,
+    address_id: int,
+    background_tasks: BackgroundTasks,
+    db: AsyncSession = Depends(get_db),
+    payload: dict = Depends(verify_token),
+):
+    return await order_service.delete_delivery_address(
+        store_id=store_id,
+        address_id=address_id,
+        background_task=background_tasks,
         db=db,
         payload=payload,
     )
