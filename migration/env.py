44a79b9f8c97f827498c69e43fe8e40091642e75
Emmanuel <emmanuelchiedueke01@gmail.com>
2026-04-07 15:54:40 +0100

    add reaction count to review and reply table

--- migration/env.py
+++ migration/env.py
@@ -1,5 +1,5 @@
 from logging.config import fileConfig
-from app.models_sql import Base
+from app.models import Base
 from sqlalchemy import engine_from_config
 from sqlalchemy import pool
 from app.database.config import settings
