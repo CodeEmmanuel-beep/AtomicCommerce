922d96f98ff7fef33becaf80873b04e43d194675
Emmanuel <emmanuelchiedueke01@gmail.com>
2026-04-20 16:23:34 +0100

    feat(store_analytics): add product_statistics and inventory_statistics routes

--- app/api/v1/routes/store_analytics.py
+++ app/api/v1/routes/store_analytics.py
@@ -1,7 +1,7 @@
 from sqlalchemy.ext.asyncio import AsyncSession
 from app.database.get import get_db
 from app.auth.verify_jwt import verify_token
-from fastapi import Depends, APIRouter
+from fastapi import Depends, APIRouter, Query
 from app.services import store_analytics_service
 
 router = APIRouter(prefix="/store_analytics", tags=["Store_Analytics"])
@@ -32,4 +32,41 @@ async def view_store_monthly_performance(
 ):
     return await store_analytics_service.view_current_performance(
         slug=slug, db=db, payload=payload
+    )
+
+
+@router.get("/product_statistics")
+async def get_products_statistics(
+    slug: str,
+    ranking: str = Query("top_product", enum=["top_product", "least_product"]),
+    time_frame: str = Query(
+        "1 week", enum=["1 month", "3 months", "6 months", "1 year", "1 week"]
+    ),
+    db: AsyncSession = Depends(get_db),
+    payload: dict = Depends(verify_token),
+):
+    return await store_analytics_service.products_stats(
+        slug=slug, ranking=ranking, time_frame=time_frame, db=db, payload=payload
+    )
+
+
+@router.get("/inventory_statistics")
+async def get_inventory_statistics(
+    slug: str,
+    stock_range: str = Query(
+        "below_ten",
+        enum=[
+            "below_thirty",
+            "below_five",
+            "below_fifty",
+            "out_of_stock",
+            "above_fifty",
+            "below_ten",
+        ],
+    ),
+    db: AsyncSession = Depends(get_db),
+    payload: dict = Depends(verify_token),
+):
+    return await store_analytics_service.inventory_stats(
+        slug=slug, stock_range=stock_range, db=db, payload=payload
     )
