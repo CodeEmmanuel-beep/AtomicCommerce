ebbb2d61b0d48c5fb431953141625825cde72905
Emmanuel <emmanuelchiedueke01@gmail.com>
2026-04-27 16:14:28 +0100

    feat(models): add is_deleted column to Category Table

--- app/models.py
+++ app/models.py
@@ -512,6 +512,7 @@ class Category(Base):
     __tablename__ = "categories"
     id: Mapped[int] = mapped_column(Integer, primary_key=True, index=True)
     name: Mapped[str] = mapped_column(String, unique=True)
+    is_deleted: Mapped[bool] = mapped_column(Boolean, default=False, index=True)
 
     products = relationship("Product", back_populates="category")
     stores = relationship("Store", back_populates="category")
@@ -525,6 +526,7 @@ class SubCategory(Base):
         Integer, ForeignKey("categories.id"), index=True
     )
     name: Mapped[str] = mapped_column(String, unique=True)
+    is_deleted: Mapped[bool] = mapped_column(Boolean, default=False, index=True)
 
     products = relationship("Product", back_populates="sub_category")
     stores = relationship("Store", back_populates="sub_category")
