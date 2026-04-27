54ed2f7cfd43365c203bc309998b53644d080ab4
Emmanuel <emmanuelchiedueke01@gmail.com>
2026-04-27 13:56:19 +0100

    refactor(category): fix typo

--- app/api/v1/routes/category.py
+++ app/api/v1/routes/category.py
@@ -13,19 +13,19 @@ router = APIRouter(prefix="/category", tags=["Category"])
 
 
 @router.post("/create_category")
-async def category(
+async def create_a_category(
     name: str, db: AsyncSession = Depends(get_db), payload: dict = Depends(verify_token)
 ):
     return await category_service.category(name=name, db=db, payload=payload)
 
 
 @router.get(
-    "/get_cart",
+    "/get_category",
     response_model=StandardResponse[PaginatedMetadata[CategoryResponse]],
     response_model_exclude_none=True,
     response_model_exclude_defaults=True,
 )
-async def retrieve(
+async def category_list(
     page: int = Query(1, ge=1),
     limit: int = Query(10, le=100),
     db: AsyncSession = Depends(get_db),
@@ -33,7 +33,7 @@ async def retrieve(
     return await category_service.retrieve(db=db, page=page, limit=limit)
 
 
-@router.delete("/delete", response_model=StandardResponse)
+@router.delete("/delete")
 async def delete_one_category(
     category_id: int,
     db: AsyncSession = Depends(get_db),
