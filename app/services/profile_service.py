cc935577f6f49753121429d28f6d8bfeb9776a8a
Emmanuel <emmanuelchiedueke01@gmail.com>
2026-04-28 13:56:36 +0100

    feat(profile_service): implement view_profile function

--- app/services/profile_service.py
+++ app/services/profile_service.py
@@ -3,6 +3,7 @@ from app.logs.logger import get_logger
 from sqlalchemy import select
 from app.models import User, Membership
 from sqlalchemy.orm import selectinload
+from app.api.v1.schemas import StandardResponse, PaginatedMetadata, UserResponse
 
 logger = get_logger("profiles")
 
@@ -14,11 +15,17 @@ async def view_profile(db, payload):
         raise HTTPException(status_code=401, detail="unauthorized access")
     profile = (
         await db.execute(
-            select(User)
-            .options(selectinload(User.membership))
+            select(User, Membership)
+            .join(Membership, User.id == Membership.user_id)
+            .options(selectinload(Membership.store))
             .where(User.id == user_id)
         )
-    ).scalar_one_or_none()
+    ).first()
     if not profile:
         logger.warning("user: %s, has no user profile in the database", user_id)
         raise HTTPException(status_code=404, detail="profile not found")
+    user, members = profile
+    membership = {mem.store.store_name: mem.membership_type for mem in members}
+    userres = UserResponse.model_validate(user)
+    userres.membership = membership
+    return StandardResponse(status="success", message="profile", data=userres)
