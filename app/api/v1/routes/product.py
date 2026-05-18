bf7bc84d1a393384b3a602fb312bb565407c50db
Emmanuel <emmanuelchiedueke01@gmail.com>
2026-05-18 21:05:29 +0100

    feat(product): add product_images endpoint

--- app/api/v1/routes/product.py
+++ app/api/v1/routes/product.py
@@ -1,4 +1,4 @@
-from fastapi import APIRouter, Depends, Query, UploadFile, File, BackgroundTasks
+from fastapi import APIRouter, Depends, Query, UploadFile, File, BackgroundTasks, Form
 from app.database.get import get_db
 from app.auth.verify_jwt import verify_token
 from sqlalchemy.ext.asyncio import AsyncSession
@@ -6,27 +6,66 @@ from app.api.v1.schemas import (
     PaginatedMetadata,
     ProductResponse,
     StandardResponse,
-    ProductObj,
 )
 from app.services import product_service
 from app.utils.supabase_url import _supabase
+from typing import List, Optional
 
 router = APIRouter(prefix="/product", tags=["Products"])
 
 
 @router.post("/add_product")
 async def create_product(
-    prod: ProductObj,
+    store_id: int = Form(...),
+    sub_category_name: str = Form(...),
     primary_image: UploadFile = File(...),
-    image: UploadFile = File(...),
+    product_name: str = Form(...),
+    product_type: str = Form(...),
+    product_size: str = Query(
+        "small", enum=["small", "medium", "large", "extra_large"]
+    ),
+    product_price: float = Form(...),
+    product_description: str = Form(...),
     db: AsyncSession = Depends(get_db),
     payload: dict = Depends(verify_token),
     get_supabase=Depends(_supabase),
 ):
     return await product_service.create(
-        prod=prod,
         primary_image=primary_image,
-        image=image,
+        store_id=store_id,
+        sub_category_name=sub_category_name,
+        product_name=product_name,
+        product_type=product_type,
+        product_size=product_size,
+        product_price=product_price,
+        product_description=product_description,
+        db=db,
+        payload=payload,
+        get_supabase=get_supabase,
+    )
+
+
+@router.post("/product_images")
+async def upload_product_images(
+    store_id: int = Form(...),
+    product_id: int = Form(...),
+    image_1: UploadFile = File(...),
+    image_2: UploadFile = File(None),
+    image_3: UploadFile = File(None),
+    image_4: UploadFile = File(None),
+    image_5: UploadFile = File(None),
+    db: AsyncSession = Depends(get_db),
+    payload: dict = Depends(verify_token),
+    get_supabase=Depends(_supabase),
+):
+    return await product_service.add_image(
+        image_1=image_1,
+        store_id=store_id,
+        product_id=product_id,
+        image_2=image_2,
+        image_3=image_3,
+        image_4=image_4,
+        image_5=image_5,
         db=db,
         payload=payload,
         get_supabase=get_supabase,
@@ -35,7 +74,6 @@ async def create_product(
 
 @router.put("/edit_product")
 async def product_change(
-    prod: ProductObj,
     primary_image: UploadFile = File(...),
     image: UploadFile = File(...),
     db: AsyncSession = Depends(get_db),
