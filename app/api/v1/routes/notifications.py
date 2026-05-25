2b46ec79d015188526290ff208d2b134230c46c3
Emmanuel <emmanuelchiedueke01@gmail.com>
2026-05-25 15:25:33 +0100

    feat(notifications): add update logic for is_read in notifications_history and get_notifications functions

--- app/api/v1/routes/notifications.py
+++ app/api/v1/routes/notifications.py
@@ -2,7 +2,7 @@ from sse_starlette.sse import EventSourceResponse
 from fastapi import APIRouter, Depends, HTTPException, Query
 from app.auth.verify_jwt import verify_token
 from app.utils.redis import notifications_stream
-from app.models import Notification
+from app.models import Notification, User
 from sqlalchemy.ext.asyncio import AsyncSession
 from app.database.get import get_db
 from app.logs.logger import get_logger
@@ -12,8 +12,9 @@ from app.api.v1.schemas import (
     StandardResponse,
     PaginatedResponse,
 )
+from typing import List
 from app.utils.redis import cache, cached, notification_invalidation
-from sqlalchemy import select, func
+from sqlalchemy import select, func, update
 
 router = APIRouter(prefix="/notifications", tags=["Notifications"])
 
@@ -36,7 +37,7 @@ async def notification_message(payload: dict = Depends(verify_token)):
 
 @router.get(
     "/notifications_list",
-    response_model=StandardResponse[NotificationResponse],
+    response_model=StandardResponse[List[NotificationResponse]],
     response_model_exclude_defaults=True,
     response_model_exclude_none=True,
 )
@@ -54,22 +55,36 @@ async def get_notifications(
         logger.info(f"Cache hit at the get_notification endpoint for user {user_id}")
         return StandardResponse(**cached_data)
     notifier = (
-        (
-            await db.execute(
-                select(Notification)
-                .where(Notification.notified_user == user_id)
-                .limit(30)
-            )
+        await db.execute(
+            select(Notification, User)
+            .join(User, Notification.from_user == User.id)
+            .where(Notification.notified_user == user_id)
+            .order_by(Notification.created_at.desc())
+            .limit(30)
         )
-        .scalars()
-        .all()
-    )
+    ).all()
     if not notifier:
-        logger.error("user %s, attempt to fetch notifications returned null")
-        return {"status": "success", "message": "no notification found"}
-    data = [NotificationResponse.model_validate(n) for n in notifier]
-    full_data = StandardResponse(status="success", message="notifications", data=data)
-    await cached(notification_key, full_data, ttl=3600)
+        logger.error("user %s, attempt to fetch notifications returned null", user_id)
+        return StandardResponse(
+            status="success", message="no notification found", data=None
+        )
+    items = []
+    for notify, sender in notifier:
+        data = NotificationResponse.model_validate(notify)
+        data.notification = (
+            f"{notify.notification} {sender.first_name} {sender.surname}"
+            if sender.is_active
+            else f"{notify.notification}" "deleted user"
+        )
+        items.append(data)
+    await db.execute(
+        update(Notification)
+        .where(Notification.notified_user == user_id)
+        .values(is_read=True)
+    )
+    await db.commit()
+    full_data = StandardResponse(status="success", message="notifications", data=items)
+    await cached(notification_key, full_data, ttl=60)
     logger.info(f"notification data cached for user {user_id}")
     return full_data
 
@@ -91,43 +106,54 @@ async def notifications_history(
         logger.warning("unauthorized attempt at get_notifications endpoint")
         raise HTTPException(status_code=401, detail="unauthorized access")
     offset = (page - 1) * limit
-    if page < 1 or limit < 1:
-        raise HTTPException(
-            status_code=400, detail="page number and limit must be greater than 0"
-        )
     notification_key = f"notification:{user_id}:{page}:{limit}"
     cached_data = await cache(notification_key)
     if cached_data:
         logger.info(f"Cache hit at the get_notification endpoint for user {user_id}")
         return StandardResponse(**cached_data)
     notifier = (
-        (
-            await db.execute(
-                select(Notification)
-                .where(Notification.notified_user == user_id)
-                .offset(offset)
-                .limit(limit)
-            )
+        await db.execute(
+            select(Notification, User)
+            .join(User, Notification.from_user == User.id)
+            .where(Notification.notified_user == user_id)
+            .order_by(Notification.created_at.desc())
+            .offset(offset)
+            .limit(limit)
         )
-        .scalars()
-        .all()
-    )
+    ).all()
     if not notifier:
-        logger.error("user %s, attempt to fetch notifications returned null")
-        return {"status": "success", "message": "no notification found"}
+        logger.error("user %s, attempt to fetch notifications returned null", user_id)
+        return StandardResponse(
+            status="success", message="no notification found", data=None
+        )
     total = (
         await db.execute(
             select(func.count(Notification.id)).where(
-                Notification.notified_user == user_id
+                Notification.notified_user == user_id, User.is_active
             )
         )
     ).scalar() or 0
     logger.info("total number of notifications for user %s is %s", user_id, total)
+    items = []
+    for notify, sender in notifier:
+        data = NotificationResponse.model_validate(notify)
+        data.notification = (
+            f"{notify.notification} {sender.first_name} {sender.surname}"
+            if sender.is_active
+            else f"{notify.notification}" "deleted user"
+        )
+        items.append(data)
+    await db.execute(
+        update(Notification)
+        .where(Notification.notified_user == user_id)
+        .values(is_read=True)
+    )
+    await db.commit()
     data = PaginatedMetadata[NotificationResponse](
-        items=[NotificationResponse.model_validate(n) for n in notifier],
+        items=items,
         pagination=PaginatedResponse(page=page, limit=limit, total=total),
     )
     full_data = StandardResponse(status="success", message="notifications", data=data)
-    await cached(notification_key, full_data, ttl=3600)
+    await cached(notification_key, full_data, ttl=60)
     logger.info(f"notification data cached for user {user_id}")
     return full_data
