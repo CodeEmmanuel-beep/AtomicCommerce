154b316663656af2275e110581e9475e78d6b32a
Emmanuel <emmanuelchiedueke01@gmail.com>
2026-05-31 17:55:54 +0100

    feat(celery_app): feat(celery_config): set worker_prefetch_multiplier=1 and enable task_acks_late

--- app/utils/celery_app.py
+++ app/utils/celery_app.py
@@ -5,9 +5,13 @@ celery_app = Celery(
     "worker",
     broker=settings.REDIS_URL,
     backend=settings.REDIS_URL,
-    include=["app.utils.celery_schedule"],
+    include=["app.utils.scheduled_task"],
 )
 
+celery_app.conf.worker_prefetch_multiplier = 1
+celery_app.conf.task_acks_late = True
+
+celery_app.conf.update()
 celery_app.conf.update(
     task_track_started=True,
     task_serializer="json",
@@ -21,8 +25,8 @@ celery_app.conf.update(
 celery_app.conf.update(
     beat_schedule={
         "cancel_order": {
-            "task": "app.utils.celery_schedule.cancel_order",
-            "schedule": 600,
+            "task": "app.utils.scheduled_task.cancel_order",
+            "schedule": 300,
         }
     }
 )
