8c3921a86d4a9c9ca517a01e98afc67e8fc5a1fc
Emmanuel <emmanuelchiedueke01@gmail.com>
2026-06-17 17:58:33 +0100

    refactor(profile): add ProfileMode model to update_profile endpoint to make parameters an object

--- app/api/v1/routes/profile.py
+++ app/api/v1/routes/profile.py
@@ -1,10 +1,9 @@
-from fastapi import APIRouter, Depends, File, UploadFile, Form
+from fastapi import APIRouter, Depends
 from app.services import profile_service
 from app.database.get import get_db
-from app.api.v1.schemas import StandardResponse, UserResponse
+from app.api.v1.schemas import StandardResponse, UserResponse, ProfileMode
 from app.auth.verify_jwt import verify_token
 from sqlalchemy.ext.asyncio import AsyncSession
-from app.utils.supabase_url import _supabase
 
 router = APIRouter(prefix="/profile", tags=["Profile"])
 
@@ -21,32 +20,18 @@ async def get_personal_profile(
     return await profile_service.view_profile(db, payload)
 
 
-@router.put("/update_profile")
+@router.put(
+    "/update_profile", response_model=StandardResponse, response_model_exclude_none=True
+)
 async def update_personal_profile(
-    profile_picture: UploadFile = File(None),
-    first_name: str = Form(None),
-    middle_name: str = Form(None),
-    surname: str = Form(None),
-    email: str = Form(None),
-    nationality: str = Form(None),
-    phone_number: str = Form(None),
-    address: str = Form(None),
+    profile: ProfileMode,
     db: AsyncSession = Depends(get_db),
     payload: dict = Depends(verify_token),
-    get_supabase=Depends(_supabase),
 ):
     return await profile_service.edit_profile(
-        profile_picture=profile_picture,
-        first_name=first_name,
-        middle_name=middle_name,
-        surname=surname,
-        email=email,
-        nationality=nationality,
-        phone_number=phone_number,
-        address=address,
+        profile=profile,
         db=db,
         payload=payload,
-        get_supabase=get_supabase,
     )
 
 
