1efea8f876759b822c3ae8652b8d7d0af63ec847
Emmanuel <emmanuelchiedueke01@gmail.com>
2026-06-17 18:00:16 +0100

    feat(auth): add profile picture endpoint

--- app/api/v1/routes/auth.py
+++ app/api/v1/routes/auth.py
@@ -1,16 +1,8 @@
-from app.api.v1.schemas import LoginResponse
+from app.api.v1.schemas import LoginResponse, StandardResponse
 from app.auth.verify_jwt import verify_token
-from fastapi import (
-    APIRouter,
-    Depends,
-    Response,
-    Request,
-    UploadFile,
-    File,
-    Query,
-    Form,
-)
+from fastapi import APIRouter, Depends, Response, Request, Query, UploadFile, File
 from sqlalchemy.ext.asyncio import AsyncSession
+from app.api.v1.schemas import RegistrationModel
 from app.database.get import get_db
 from app.services import auth_service
 from app.utils.supabase_url import _supabase
@@ -18,31 +10,33 @@ from app.utils.supabase_url import _supabase
 router = APIRouter(prefix="/auth", tags=["Authentication"])
 
 
-@router.post("/register")
+@router.post(
+    "/register", response_model=StandardResponse, response_model_exclude_none=True
+)
 async def registration(
-    first_name: str = Form(...),
-    surname: str = Form(...),
-    username: str = Form(...),
-    email: str = Form(...),
-    nationality: str = Form(...),
-    address: str = Form(None),
-    password: str = Form(...),
-    confirm_password: str = Form(...),
-    profile_picture: UploadFile | None = File(None),
-    db: AsyncSession = Depends(get_db),
-    get_supabase=Depends(_supabase),
+    registration: RegistrationModel, db: AsyncSession = Depends(get_db)
 ):
     return await auth_service.reg(
-        first_name=first_name,
-        surname=surname,
-        username=username,
-        email=email,
-        nationality=nationality,
-        address=address,
-        password=password,
-        confirm_password=confirm_password,
+        registration=registration,
+        db=db,
+    )
+
+
+@router.post(
+    "/profile_picture",
+    response_model=StandardResponse,
+    response_model_exclude_none=True,
+)
+async def upload(
+    profile_picture: UploadFile = File(...),
+    db: AsyncSession = Depends(get_db),
+    payload: dict = Depends(verify_token),
+    get_supabase=Depends(_supabase),
+):
+    return await auth_service.upload_profile_picture(
         profile_picture=profile_picture,
         db=db,
+        payload=payload,
         get_supabase=get_supabase,
     )
 
