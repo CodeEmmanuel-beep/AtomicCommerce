fa58132c3708816415592f5773753f7f87fbd366
Emmanuel <emmanuelchiedueke01@gmail.com>
2026-06-26 22:29:52 +0100

    refactor(product_reviews): add response model configuration to endpoint routers

--- app/api/v1/routes/product_reviews.py
+++ app/api/v1/routes/product_reviews.py
@@ -1,4 +1,4 @@
-from fastapi import APIRouter, Depends, Query
+from fastapi import APIRouter, Depends, Query, BackgroundTasks
 from app.auth.verify_jwt import verify_token
 from app.database.get import get_db
 from sqlalchemy.ext.asyncio import AsyncSession
@@ -13,14 +13,24 @@ from app.api.v1.schemas import (
 router = APIRouter(prefix="/product_reviews", tags=["Product_Reviews"])
 
 
-@router.post("/post_product_review")
+@router.post(
+    "/post_product_review",
+    response_model=StandardResponse,
+    response_model_exclude_none=True,
+)
 async def create_product_review(
     review: Review,
+    background_task: BackgroundTasks,
+    ratings: int = Query(0, le=5),
     db: AsyncSession = Depends(get_db),
     payload: dict = Depends(verify_token),
 ):
     return await product_reviews_service.product_review(
-        review=review, db=db, payload=payload
+        review=review,
+        background_task=background_task,
+        ratings=ratings,
+        db=db,
+        payload=payload,
     )
 
 
@@ -41,23 +51,38 @@ async def product_review_list(
     )
 
 
-@router.put("/edit_product_review")
+@router.put(
+    "/edit_product_review",
+    response_model=StandardResponse,
+    response_model_exclude_none=True,
+)
 async def product_review_update(
     review: Review,
+    background_task: BackgroundTasks,
+    ratings: int = Query(0, le=5),
     db: AsyncSession = Depends(get_db),
     payload: dict = Depends(verify_token),
 ):
     return await product_reviews_service.update_review(
-        review=review, db=db, payload=payload
+        review=review,
+        ratings=ratings,
+        background_task=background_task,
+        db=db,
+        payload=payload,
     )
 
 
-@router.delete("/product_review_delete/{product_id}")
+@router.delete(
+    "/product_review_delete/{product_id}",
+    response_model=StandardResponse,
+    response_model_exclude_none=True,
+)
 async def delete_product_review(
     product_id: int,
+    background_task: BackgroundTasks,
     db: AsyncSession = Depends(get_db),
     payload: dict = Depends(verify_token),
 ):
     return await product_reviews_service.delete_review(
-        product_id=product_id, db=db, payload=payload
+        product_id=product_id, background_task=background_task, db=db, payload=payload
     )
