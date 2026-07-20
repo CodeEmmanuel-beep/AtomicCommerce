20b6ba2cb70ed9eeca0c0617531131321e44de43
Emmanuel <emmanuelchiedueke01@gmail.com>
2026-07-20 19:05:34 +0100

    feat(reactions): add reactions_list endpoint

--- app/api/v1/routes/reactions.py
+++ app/api/v1/routes/reactions.py
@@ -1,11 +1,16 @@
 from sqlalchemy.ext.asyncio import AsyncSession
-from fastapi import APIRouter, Depends, BackgroundTasks
+from fastapi import APIRouter, Depends, BackgroundTasks, Query
 from app.database.get import get_db
-from app.api.v1.schemas import ReactionType
+from app.api.v1.schemas import (
+    ReactionType,
+    ReactResponse,
+    StandardResponse,
+    PaginatedMetadata,
+)
 from app.auth.verify_jwt import verify_token
 from app.services import reactions_service
 
-router = APIRouter(prefix="/react", tags=["Reactions"])
+router = APIRouter(prefix="/reactions", tags=["Reactions"])
 
 
 @router.post("/react")
@@ -22,6 +27,30 @@ async def react_type(
         background_task=background_task,
         reply_id=reply_id,
         review_id=review_id,
+        db=db,
+        payload=payload,
+    )
+
+
+@router.get(
+    "reactions_list",
+    response_model=StandardResponse[PaginatedMetadata[ReactResponse]],
+    response_model_exclude_none=True,
+    response_model_exclude_defaults=True,
+)
+async def get_reactions(
+    review_id: int | None = None,
+    reply_id: int | None = None,
+    page: int = Query(1, ge=1),
+    limit: int = Query(10, le=100),
+    db: AsyncSession = Depends(get_db),
+    payload: dict = Depends(verify_token),
+):
+    return await reactions_service.view_reactions(
+        review_id=review_id,
+        reply_id=reply_id,
+        page=page,
+        limit=limit,
         db=db,
         payload=payload,
     )
