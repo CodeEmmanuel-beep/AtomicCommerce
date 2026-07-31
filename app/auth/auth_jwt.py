5790f0a9a3246e95a3ba225897895c09e5ab6a73
Emmanuel <emmanuelchiedueke01@gmail.com>
2026-07-31 22:33:15 +0100

    feat(auth_jwt): split access token and refresh token jwt encode keys for better security

--- app/auth/auth_jwt.py
+++ app/auth/auth_jwt.py
@@ -1,24 +1,9 @@
 from jose import jwt
 from fastapi import HTTPException
 from datetime import datetime, timezone, timedelta
-from dotenv import load_dotenv
 from passlib.context import CryptContext
-import os
+from app.database.config import settings
 import re
-
-load_dotenv()
-sk = os.getenv("SECRET_KEY")
-if not sk:
-    raise RuntimeError("Could not access SECRET_KEY")
-SECRET_KEY = sk
-al = os.getenv("ALGORITHM")
-if not al:
-    raise RuntimeError("Could not access ALGORITH")
-ALGORITHM = al
-minutes_env = os.getenv("ACCESS_TOKEN_MINUTES")
-if not minutes_env:
-    raise RuntimeError("Could not access ACCESS_TOKEN_MINUTES")
-ACCESS_TOKEN_MINUTES = int(minutes_env)
 
 password_context = CryptContext(schemes=["argon2"], deprecated="auto")
 
@@ -43,10 +28,10 @@ def create_access_token(data: dict, expire_delta: timedelta | None = None):
     to_encode = data.copy()
     to_encode["type"] = "access_token"
     expire = datetime.now(timezone.utc) + (
-        expire_delta or timedelta(minutes=ACCESS_TOKEN_MINUTES)
+        expire_delta or timedelta(minutes=settings.ACCESS_TOKEN_MINUTES)
     )
     to_encode.update({"exp": expire})
-    return jwt.encode(to_encode, SECRET_KEY, algorithm=ALGORITHM)
+    return jwt.encode(to_encode, settings.SECRET_KEY, algorithm=settings.ALGORITHM)
 
 
 def create_refresh_token(data: dict, expire_delta: timedelta | None = None):
@@ -54,4 +39,4 @@ def create_refresh_token(data: dict, expire_delta: timedelta | None = None):
     to_encode["type"] = "refresh_token"
     expire = datetime.now(timezone.utc) + (expire_delta or timedelta(days=7))
     to_encode.update({"exp": expire})
-    return jwt.encode(to_encode, SECRET_KEY, algorithm=ALGORITHM)
+    return jwt.encode(to_encode, settings.REFRESH_KEY, algorithm=settings.ALGORITHM)
