b90710447c6f3902e7088b9c70cfb6fdc9a50fc5
Emmanuel <emmanuelchiedueke01@gmail.com>
2026-08-01 19:27:48 +0100

    chore(get): add async database dependency injection

--- app/database/get.py
+++ app/database/get.py
@@ -1,6 +1,10 @@
+from fastapi import Depends
 from app.database.async_config import AsyncSessionLocal
 
 
 async def get_db():
     async with AsyncSessionLocal() as session:
         yield session
+
+
+async_db = Depends(get_db)
