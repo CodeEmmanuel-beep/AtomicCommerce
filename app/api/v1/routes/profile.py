30ac5f8b2ba3b524cb12d2ec54d09959f27a9bc1
Emmanuel <emmanuelchiedueke01@gmail.com>
2026-08-08 17:13:58 +0100

    feat(profile): add general_profile endpoint

--- app/api/v1/routes/profile.py
+++ app/api/v1/routes/profile.py
@@ -1,10 +1,22 @@
-from fastapi import APIRouter, Request, Response, Query
+from fastapi import APIRouter, Request, Response, Query, Depends
 from app.services import profile_service
-from app.database.get import async_db
-from app.api.v1.schemas import StandardResponse, UserResponse, ProfileMode
+from typing import Annotated
+from app.database.get import get_db
+from app.api.v1.schemas import (
+    StandardResponse,
+    UserResponse,
+    ProfileMode,
+    QueryEnum,
+    SuperUserResponse,
+    PaginatedMetadata,
+    UserState,
+)
+from app.models import BanUnit
 from sqlalchemy.ext.asyncio import AsyncSession
 
 router = APIRouter(prefix="/profile", tags=["Profile"])
+
+DatabaseDep = Annotated[AsyncSession, Depends(get_db)]
 
 
 @router.get(
@@ -13,25 +25,39 @@ router = APIRouter(prefix="/profile", tags=["Profile"])
     response_model_exclude_none=True,
     response_model_exclude_defaults=True,
 )
-async def get_personal_profile(request: Request, db: AsyncSession = async_db):
+async def get_personal_profile(request: Request, db: DatabaseDep):
     return await profile_service.view_profile(db=db, request=request)
+
+
+@router.get(
+    "/general_profile",
+    response_model=StandardResponse[PaginatedMetadata[SuperUserResponse]],
+    response_model_exclude_none=True,
+    response_model_exclude_defaults=True,
+)
+async def view_general_profile(
+    request: Request,
+    db: DatabaseDep,
+    user_state: UserState = Query(None),
+    cursor_id: int | None = Query(None, ge=1),
+    limit: int = Query(10, ge=1, le=100),
+):
+    return await profile_service.view_profiles(
+        request=request, cursor_id=cursor_id, db=db, state=user_state, limit=limit
+    )
 
 
 @router.put(
     "/update_profile", response_model=StandardResponse, response_model_exclude_none=True
 )
 async def update_personal_profile(
-    request: Request, profile: ProfileMode, db: AsyncSession = async_db
+    request: Request, profile: ProfileMode, db: DatabaseDep
 ):
     return await profile_service.edit_profile(profile=profile, db=db, request=request)
 
 
 @router.delete("/deactivate_personal_profile")
-async def profile_deactivation(
-    request: Request,
-    response: Response,
-    db: AsyncSession = async_db,
-):
+async def profile_deactivation(request: Request, response: Response, db: DatabaseDep):
     return await profile_service.deactivate_profile(
         db=db, response=response, request=request
     )
@@ -40,19 +66,19 @@ async def profile_deactivation(
 @router.delete("/ban_profile")
 async def profile_ban(
     request: Request,
+    db: DatabaseDep,
     userId: int,
     ban_period: int | None = None,
     ban_reason: str | None = None,
-    indefinite: str = Query("No", enum=["Yes", "No"]),
-    ban_unit: str | None = Query("days", enum=["months", None, "days"]),
-    db: AsyncSession = async_db,
+    indefinite: QueryEnum = Query(QueryEnum.No),
+    ban_unit: BanUnit = Query(None),
 ):
     return await profile_service.ban_profile(
         userId=userId,
         db=db,
         ban_period=ban_period,
-        ban_unit=ban_unit,
-        indefinite=indefinite,
+        ban_unit=ban_unit.value if ban_unit else None,
+        indefinite=indefinite.value,
         request=request,
         ban_reason=ban_reason,
     )
