d1bd46e63bebc8c43c423c7b817abd3d9f8456e0
Emmanuel <emmanuelchiedueke01@gmail.com>
2026-08-14 17:38:51 +0100

    feat(notifications): add DatabaseDep annotation

--- app/api/v1/routes/notifications.py
+++ app/api/v1/routes/notifications.py
@@ -1,11 +1,13 @@
-from fastapi import APIRouter, Request, Query
+from fastapi import APIRouter, Request, Query,Depends
 from sqlalchemy.ext.asyncio import AsyncSession
-from app.database.get import async_db
+from app.database.get import get_db
 from app.api.v1.schemas import NotificationResponse, PaginatedMetadata, StandardResponse
 from app.services import notification_service
+from typing import Annotated
 
 router = APIRouter(prefix="/notifications", tags=["Notifications"])
 
+DatabaseDep = Annotated[AsyncSession, Depends(get_db)]
 
 @router.get("/notice")
 async def notification_message(request: Request):
@@ -18,7 +20,7 @@ async def notification_message(request: Request):
     response_model_exclude_defaults=True,
     response_model_exclude_none=True,
 )
-async def get_notifications(request: Request, db: AsyncSession = async_db):
+async def get_notifications(request: Request, db:DatabaseDep):
     return await notification_service.retrieve_notifications(request=request, db=db)
 
 
@@ -29,10 +31,9 @@ async def get_notifications(request: Request, db: AsyncSession = async_db):
     response_model_exclude_none=True,
 )
 async def notifications_history(
-    request: Request,
+    request: Request,db:DatabaseDep,
     page: int = Query(1, ge=1),
-    limit: int = Query(10, le=100),
-    db: AsyncSession = async_db,
+    limit: int = Query(10, ge=1, le=100)
 ):
     return await notification_service.notifications_list(
         page=page, limit=limit, db=db, request=request
