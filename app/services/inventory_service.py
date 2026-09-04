6080a72a649a275718855d27e2556e02db56fbd0
Emmanuel <emmanuelchiedueke01@gmail.com>
2026-09-04 21:01:07 +0100

    feat(inventory_service): add read_prod_inventory handler

--- app/services/inventory_service.py
+++ app/services/inventory_service.py
@@ -1,23 +1,30 @@
-from app.models import Inventory, Product, store_owners, store_staffs
+from app.models import Inventory, Product, store_owners, store_staffs, ProductVariant
 from sqlalchemy.exc import IntegrityError
 from app.api.v1.schemas import (
     StandardResponse,
     PaginatedMetadata,
     PaginatedResponse,
+    ProductRes,
     InventoryResponse,
 )
 from fastapi import HTTPException, Response, status
-from app.utils.helper import store_auth, store_inventory
+from app.utils.helper import store_auth, store_inventory, unique_id
 from app.logs.logger import get_logger
 from sqlalchemy import select, exists, func
-from sqlalchemy.orm import selectinload
-from app.utils.redis import cache, cached
+from sqlalchemy.orm import selectinload, contains_eager
+from app.utils.redis import (
+    cache,
+    cached,
+    product_version_invalidation,
+    product_variant_invalidation,
+    product_variants_invalidation,
+)
 
 logger = get_logger("inventory")
 
 
-async def create(store_id: int, product_id: int, stock_quantity: int, db, payload):
-    user_id = payload.get("user_id")
+async def create(store_id, request, variant_id, stock_quantity, db, background_task):
+    user_id = unique_id(request)
     if not user_id:
         logger.warning("unauthorized attempt at the create endpoint")
         raise HTTPException(status_code=401, detail="not authenticated")
@@ -30,26 +37,61 @@ async def create(store_id: int, product_id: int, stock_quantity: int, db, payloa
             store_staffs.c.stores_id == store_id,
             store_staffs.c.users_id == user_id,
         ),
-        exists().where(
-            Product.id == product_id, Product.store_id == store_id, ~Product.is_deleted
+        exists(
+            select(1)
+            .select_from(ProductVariant)
+            .join(Product, ProductVariant.product_id == Product.id)
+            .where(
+                ProductVariant.is_deleted.is_(False),
+                ProductVariant.id == variant_id,
+                Product.store_id == store_id,
+                Product.is_deleted.is_(False),
+            )
         ),
+        exists().where(Inventory.variant_id == variant_id),
     )
-    result = (await db.execute(stmt)).fetchone() or (False, False, False)
-    owner, staff, product_verified = result
+    result = (await db.execute(stmt)).fetchone() or (False, False, False, False)
+    owner, staff, productvariant_verified, already_stocked = result
     if not owner and not staff:
         logger.warning(
             "user: %s, made an ineligible attempt in create inventory endpoint", user_id
         )
         raise HTTPException(status_code=403, detail="ineligible credentials")
-    if not product_verified:
+    if not productvariant_verified:
         logger.warning(
-            "user: %s tried creating inventory for invalid or cross-tenant product_id: %s",
+            "user: %s tried creating inventory for invalid or cross-tenant variant_id: %s",
             user_id,
-            product_id,
+            variant_id,
         )
-        raise HTTPException(status_code=404, detail="product not found in this store")
+        raise HTTPException(
+            status_code=404, detail="product variant not found in this store"
+        )
+    if already_stocked:
+        logger.warning(
+            "user: %s tried duplicating inventory for variant: %s", user_id, variant_id
+        )
+        raise HTTPException(status_code=400, detail="stock already exists")
+    product_update = (
+        await db.execute(
+            select(Product)
+            .join(ProductVariant, Product.id == ProductVariant.product_id)
+            .where(ProductVariant.id == variant_id, Product.is_deleted.is_(False))
+        )
+    ).scalar_one_or_none()
+    if not product_update:
+        logger.warning(
+            "user: %s tried creating inventory for invalid or cross-tenant variant_id: %s",
+            user_id,
+            variant_id,
+        )
+        raise HTTPException(
+            status_code=404, detail="product variant not found in this store"
+        )
+    if product_update.product_availability == "out_of_stock":
+        product_update.product_availability = "available"
+    product_id = product_update.id
     stock = Inventory(
-        store_id=store_id, product_id=product_id, stock_quantity=stock_quantity
+        store_id=store_id, variant_id=variant_id, stock_quantity=stock_quantity
     )
     try:
         db.add(stock)
@@ -70,12 +112,15 @@ async def create(store_id: int, product_id: int, stock_quantity: int, db, payloa
             user_id,
         )
         raise HTTPException(status_code=500, detail="internal server error")
-    logger.info("inventory created successfully for product: %s", product_id)
+    background_task.add_task(product_variants_invalidation, product_id)
+    background_task.add_task(product_variant_invalidation, variant_id)
+    background_task.add_task(product_version_invalidation, product_id)
+    logger.info("inventory created successfully for variant: %s", variant_id)
     return StandardResponse(status="success", message="inventory created", data=None)
 
 
-async def read(store_id, inventory_id, db, payload):
-    user_id = await store_auth(store_id, db, payload)
+async def read(store_id, request, inventory_id, db):
+    user_id = await store_auth(store_id, db, request)
     stmt = store_inventory(store_id, inventory_id)
     cache_key = f"inventory:{store_id}:{inventory_id}"
     inventory_cache = await cache(cache_key)
@@ -103,8 +148,71 @@ async def read(store_id, inventory_id, db, payload):
     return response
 
 
-async def read_all(store_id, page, limit, db, payload):
-    user_id = await store_auth(store_id, db, payload)
+async def read_prod_inventory(store_id, product_id, request, page, limit, db):
+    user_id = await store_auth(store_id, db, request)
+    offset = (page - 1) * limit
+    cache_key = f"inventory_list:{store_id}:{product_id}:{page}:{limit}"
+    inventory_cache = await cache(cache_key)
+    if inventory_cache:
+        logger.info(
+            "inventory cache hit for store_id: %s product_id %s", store_id, product_id
+        )
+        return StandardResponse(**inventory_cache)
+    stmt = (
+        select(Inventory)
+        .join(ProductVariant, Inventory.variant_id == ProductVariant.id)
+        .join(Product, ProductVariant.product_id == Product.id)
+        .options(contains_eager(Inventory.variant).selectinload(ProductVariant.product))
+        .where(
+            ProductVariant.product_id == product_id,
+            ProductVariant.is_deleted.is_(False),
+            Inventory.is_deleted.is_(False),
+            Product.is_deleted.is_(False),
+            Inventory.store_id == store_id,
+        )
+    )
+    results = (
+        (
+            await db.execute(
+                stmt.order_by(Inventory.last_updated.desc()).offset(offset).limit(limit)
+            )
+        )
+        .scalars()
+        .all()
+    )
+    if not results:
+        logger.warning("user: %s, tried fetching a non existent inventory", user_id)
+        raise HTTPException(status_code=404, detail="inventory not found")
+    total = (
+        await db.execute(
+            select(func.count(Inventory.id))
+            .join(ProductVariant, Inventory.variant_id == ProductVariant.id)
+            .where(
+                Inventory.store_id == store_id,
+                Inventory.is_deleted.is_(False),
+                ProductVariant.product_id == product_id,
+                ProductVariant.is_deleted.is_(False),
+            )
+        )
+    ).scalar() or 0
+    logger.info("total inventories for store: %s, is: %s", store_id, total)
+    product = ProductRes.model_validate(results[0].variant.product)
+    data = PaginatedMetadata[InventoryResponse](
+        items=[InventoryResponse.model_validate(r) for r in results],
+        pagination=PaginatedResponse(page=page, limit=limit, total=total),
+    )
+    full_response = StandardResponse(
+        status="success",
+        message="product inventory",
+        data={"product": product, "stocks": data},
+    )
+    await cached(cache_key, full_response, ttl=60)
+    logger.info("read_all function returned data for user %s", user_id)
+    return full_response
+
+
+async def read_all(store_id, request, page, limit, db):
+    user_id = await store_auth(store_id, db, request)
     offset = (page - 1) * limit
     cache_key = f"inventory_list:{store_id}:{page}:{limit}"
     inventory_cache = await cache(cache_key)
@@ -127,7 +235,7 @@ async def read_all(store_id, page, limit, db, payload):
     total = (
         await db.execute(
             select(func.count(Inventory.id)).where(
-                Inventory.store_id == store_id, ~Inventory.is_deleted
+                Inventory.store_id == store_id, Inventory.is_deleted.is_(False)
             )
         )
     ).scalar() or 0
@@ -144,10 +252,12 @@ async def read_all(store_id, page, limit, db, payload):
     return full_response
 
 
-async def update(store_id, inventory_id, stock_quantity, db, payload):
-    user_id = await store_auth(store_id, db, payload)
+async def update(store_id, inventory_id, stock_quantity, db, request, background_task):
+    user_id = await store_auth(store_id, db, request)
     stmt = store_inventory(store_id, inventory_id)
-    stmt = stmt.options(selectinload(Inventory.product)).with_for_update()
+    stmt = stmt.options(
+        selectinload(Inventory.variant).selectinload(ProductVariant.product)
+    ).with_for_update()
     inventory = (await db.execute(stmt)).scalar_one_or_none()
     if not inventory:
         logger.warning("user: %s, tried updating a non existent inventory", user_id)
@@ -155,8 +265,10 @@ async def update(store_id, inventory_id, stock_quantity, db, payload):
     if inventory.stock_quantity == stock_quantity:
         return Response(status_code=status.HTTP_204_NO_CONTENT)
     inventory.stock_quantity = stock_quantity
-    if inventory.product.product_availability == "out_of_stock":
-        inventory.product.product_availability = "available"
+    if inventory.variant.product.product_availability == "out_of_stock":
+        inventory.variant.product.product_availability = "available"
+    product_id = inventory.variant.product.id
+    variant_id = inventory.variant.id
     try:
         await db.commit()
     except IntegrityError:
@@ -175,39 +287,8 @@ async def update(store_id, inventory_id, stock_quantity, db, payload):
             user_id,
         )
         raise HTTPException(status_code=500, detail="internal server error")
+    background_task.add_task(product_variants_invalidation, product_id)
+    background_task.add_task(product_variant_invalidation, variant_id)
+    background_task.add_task(product_version_invalidation, product_id)
     logger.info("inventory: %s, updated successfully", inventory_id)
     return StandardResponse(status="success", message="inventory updated", data=None)
-
-
-async def delete(store_id: int, inventory_id: int, db, payload):
-    user_id = await store_auth(store_id, db, payload)
-    stmt = store_inventory(store_id, inventory_id)
-    stmt = stmt.with_for_update()
-    inventory = (await db.execute(stmt)).scalar_one_or_none()
-    if not inventory:
-        logger.warning(
-            "user: %s, tried deleting a non existent inventory",
-            user_id,
-        )
-        raise HTTPException(status_code=404, detail="inventory not found")
-    inventory.is_deleted = True
-    try:
-        await db.commit()
-    except IntegrityError:
-        await db.rollback()
-        logger.error(
-            "database error occurred while deleting inventory for store: %s, user affected '%s'",
-            store_id,
-            user_id,
-        )
-        raise HTTPException(status_code=400, detail="database error")
-    except Exception:
-        await db.rollback()
-        logger.exception(
-            "error occurred while deleting inventory for store: %s, user affected '%s'",
-            store_id,
-            user_id,
-        )
-        raise HTTPException(status_code=500, detail="internal server error")
-    logger.info("inventory: %s, successfully deleted", inventory_id)
-    return StandardResponse(status="success", message="inventory deleted", data=None)
